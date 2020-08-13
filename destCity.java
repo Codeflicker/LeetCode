f0d57439d5ab3d2090cbee659f57dec73252baff
Mehtab Alam <34057403+Mehtab-alam-android@users.noreply.github.com>
2020-08-13 22:03:08 +0530

    Update destCity.java

--- destCity.java
+++ destCity.java
@@ -4,15 +4,12 @@ class Solution {
         HashMap<String,String> route = new HashMap<>();
         for(List<String> place :paths){
             route.put(place.get(0),place.get(1));
-
                 start = place.get(0);
             }
         }
-        
         while(route.get(start) != null){
             start = route.get(start);
         }
-        
         return start;
     }
 }
